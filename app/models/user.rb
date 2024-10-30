270f8163c7d926d07e13fb3dd05def4a8b74536e
inayukaina <inayukaina@gmail.com>
2024-10-30 21:28:56 +0900

    エラーメッセージの表示を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  validates :password, length: { minimum: 6 }
 end
