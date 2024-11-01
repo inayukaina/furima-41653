563a2738c1aa19790e38d34525aad2f639a14fde
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-11-01 15:17:51 +0900

    Active Storageのアソシエーションとストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :image).merge(user_id: current_user.id)
+  end
 end
