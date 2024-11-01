200d75e6185690ea709f30392d7f68924e4dd1ec
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-11-01 18:21:19 +0900

    ActiveHashの導入と設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :item_condition_id, :shipping_cost_status_id, :prefecture_id,
+                                 :shipping_schedule_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_cost_status
+  belongs_to :prefecture
+  belongs_to :shipping_schedule
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_schedule_id, numericality: { other_than: 1, message: "can't be blank" }
 end
