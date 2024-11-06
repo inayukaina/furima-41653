5f3aa7c1672d47939c79dcaecf38a0fae343ceb7
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-11-06 14:41:18 +0900

    出品者のみ編集画面へ遷移可能に制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    return unless current_user != @item.user
+
+    redirect_to root_path
   end
 
   def update
