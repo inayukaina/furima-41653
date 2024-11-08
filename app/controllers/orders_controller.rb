4702a06837df87d8f75dba7bd81408ac8069c301
inayukaina <inayukaina@gmail.com>
2024-11-08 16:49:48 +0900

    Formオブジェクトの実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :new, :create]
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building,
+                                          :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number
+  end
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address,
+                   building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
